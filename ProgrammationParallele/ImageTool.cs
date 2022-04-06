06786c3303f871600d5b6c7903efd72d8b71d612
Yannk <yann.kerleguer@orange.fr>
2022-04-06 09:48:18 +0200

    ajout gestion des images

--- ProgrammationParallele/ImageTool.cs
+++ ProgrammationParallele/ImageTool.cs
@@ -1,13 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace ProgrammationParallele
 {
     public class ImageTool
     {
+        public string filePathSource = "./Images_source/";
+        public string filePathResult = "./Images_result/";
 
+        public Image loadImage(string name)
+        {
+            Image img = Image.FromFile(string.Concat(filePathSource, $"/{name}.jpg"));
+
+            return img;
+        }
+
+        public Bitmap changeInBlackAndWith(Bitmap original)
+        {
+
+            Bitmap output = new Bitmap(original.Width, original.Height);
+
+            for (int i = 0; i < original.Width; i++)
+            {
+
+                for (int j = 0; j < original.Height; j++)
+                {
+
+                    Color c = original.GetPixel(i, j);
+
+                    int average = ((c.R + c.B + c.G) / 3);
+
+                    if (average < 200)
+                        output.SetPixel(i, j, Color.Black);
+
+                    else
+                        output.SetPixel(i, j, Color.White);
+
+                }
+            }
+
+            return output;
+
+        }
+
+        public void resizeImage(Image imgToResize, Size size, string fileName)
+        {
+            //Get the image current width
+            int sourceWidth = imgToResize.Width;
+            //Get the image current height
+            int sourceHeight = imgToResize.Height;
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            //Calulate width with new desired size
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            //Calculate height with new desired size
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+            //New Width
+            int destWidth = (int)(sourceWidth * nPercent);
+            //New Height
+            int destHeight = (int)(sourceHeight * nPercent);
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            // Draw image with new width and height
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            b.Save($"{filePathResult + fileName}.jpeg", ImageFormat.Jpeg);
+        }
     }
 }
--- ProgrammationParallele/Program.cs
+++ ProgrammationParallele/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ProgrammationParallele;
+
 Console.WriteLine("Hello, World!");
+
+var imageTools = new ImageTool();
+string name = "364376";
+imageTools.resizeImage(imageTools.loadImage(name),new System.Drawing.Size(500,100), name);
+
